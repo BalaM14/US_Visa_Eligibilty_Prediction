390109121595515b2beddd1633a4b964c550a342
BalaM14 <balamurugan141196@gmail.com>
2024-07-12 14:52:51 +0530

    Credentials Updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -67,7 +67,7 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisa-bucket14"
+MODEL_BUCKET_NAME = "usvisa-buck"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
